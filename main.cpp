70a7161637f2e3bf45dc7d9cb750bbf17a13fa7c
Ma Yingkui <maeikei@gmail.com>
2016-11-11 16:47:16 +0900

    kana to wav

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
-class watorNN {
+class WatorBaseL {
 public:
-  watorNN();
+protected:
+  WatorBaseL();
 };
+
+class WatorInputL :public WatorBaseL {
+public:
+  WatorInputL();
+protected:
+  vector<WatorBaseL> top_;
+};
+
+class WatorOutputL :public WatorBaseL {
+public:
+  WatorOutputL();
+protected:
+  vector<WatorBaseL> buttom_;
+};
+
+class WatorHiddenL :public WatorBaseL {
+public:
+  WatorHiddenL();
+protected:
+  vector<WatorBaseL> top_;
+  vector<WatorBaseL> buttom_;
+};
+
 
 int main() {
   return 0;
